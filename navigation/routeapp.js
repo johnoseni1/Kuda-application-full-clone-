c7a2e0953026d5a3c6de94aac5ee7f946b07d63f
John Oseni <johnoseni26@gmail.com>
2022-01-28 10:52:34 +0100

    added vector icons

--- navigation/routeapp.js
+++ navigation/routeapp.js
@@ -26,3 +26,5 @@ import { AsyncStorage } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
