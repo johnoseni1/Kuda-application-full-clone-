d3ae298e539bc90cf5ac45eeb2a309adeff1e06f
John Oseni <johnoseni26@gmail.com>
2022-01-28 18:11:51 +0100

    added the budget.js

---
+++ Screens/budget.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+class budget extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Budgets</Text>
+      </View>
+    );
+  }
+}
+
+export default budget;
