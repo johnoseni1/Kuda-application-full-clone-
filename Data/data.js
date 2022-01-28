f534bc966ef182064a7e80f333b85fb0a8e39054
John Oseni <johnoseni26@gmail.com>
2022-01-28 10:39:40 +0100

    created a sample data

---
+++ Data/data.js
@@ -0,0 +1,28 @@
+export const DataUsed = [
+    {
+      id: 1,
+      image: require('../asset/illustration/undraw_wallet_aym5.svg.png'),
+      titleImage: require('../asset/illustration/bank.png'),
+      title: 'Bank',
+      description:
+        'A smart bank with fatures that helps you achieve financial freedom',
+      btnName: 'Next',
+    },
+    {
+      id: 2,
+      image: require('../asset/illustration/undraw_Savings_dwkw.svg.png'),
+      titleImage: require('../asset/illustration/save.png'),
+      title: 'Save',
+      description: 'Reach your saving goal with kuda',
+      btnName: 'Next',
+    },
+    {
+      id: 3,
+      image: require('../asset/illustration/undraw_Credit_card_3ed6.svg.png'),
+      titleImage: require('../asset/illustration/credit.png'),
+      title: 'Credit',
+      description: 'Get short term loans at low interest rate',
+      btnName: 'Got it',
+    },
+  ];
+  
