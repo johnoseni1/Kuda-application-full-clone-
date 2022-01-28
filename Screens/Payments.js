6dc8a366f0dbc6894d20c61627b3b0f79d76938f
John Oseni <johnoseni26@gmail.com>
2022-01-28 12:14:54 +0100

    the splash screen

---
+++ Screens/Payments.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+class payments extends React.Component {
+
+  render() {
+
+    return (
+        
+      <View>
+        <Text>Payments</Text>
+      </View>
+
+    );
+
+  }
+  
+}
+
+export default payments;payments;
