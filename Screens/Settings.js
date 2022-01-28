7577a843bcf03c72b0c4ca3b2e7bdecd628e1677
John Oseni <johnoseni26@gmail.com>
2022-01-28 11:56:39 +0100

    the splash screen

---
+++ Screens/Settings.js
@@ -0,0 +1,19 @@
+
+
+// import react in your project
+
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+class settings extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Settings</Text>
+            </View>
+        )
+    }
+}
+
+export default settings;settings
