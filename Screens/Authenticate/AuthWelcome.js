5bc3dc6e7de38c0e9aa7c1c1bdaa6f93bdcc4e05
John Oseni <johnoseni26@gmail.com>
2022-01-29 22:28:38 +0100

    authwelcome

--- Screens/Authenticate/AuthWelcome.js
+++ Screens/Authenticate/AuthWelcome.js
@@ -20,3 +20,94 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+import React from 'react';
+import {View, Text, Image, StyleSheet, Dimensions} from 'react-native';
+import KudaBtn from '../components/Button';
+import QusOpt from '../components/QusOpt';
+
+const {width, height} = Dimensions.get('window');
+
+class AuthWelcome extends React.Component {
+  render() {
+    const {navigation} = this.props;
+    return (
+      <View style={style.main}>
+        <View>
+          <Text style={style.logoText}>!Kuda.</Text>
+        </View>
+        <View style={style.illustration}>
+          <Image
+            style={style.illustrationImage}
+            source={require('../../asset/illustration/promo-support-img.png')}
+          />
+        </View>
+        <View />
+        <View style={style.welcomeText}>
+          <Text style={style.welcomeTextTop}>Welcome to your</Text>
+          <Text style={style.welcomeTextBottom}>Play time.</Text>
+        </View>
+        <KudaBtn
+          btnName={'Join Kuda'}
+          onPress={() => navigation.navigate('Signup')}
+        />
+        <QusOpt
+          question={'Have an Account'}
+          option={'Sign in'}
+          onPress={() => navigation.navigate('Signin')}
+        />
+      </View>
+    );
+  }
+}
+
+const style = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  logoText: {
+    color: '#40196d',
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  illustration: {
+    alignItems: 'center',
+  },
+  illustrationImage: {
+    width,
+    height: height / 2,
+    resizeMode: 'contain',
+  },
+  welcomeText: {
+    alignItems: 'center',
+  },
+  welcomeTextTop: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#40196d',
+  },
+  welcomeTextBottom: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#40196d',
+  },
+  signin: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  signinQuestion: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+  signinOption: {},
+  signinOptionText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#48d38a',
+  },
+});
+
+export default AuthWelcome;
